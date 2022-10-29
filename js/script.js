f4d053ac7bca4d6c99865c3ef27085de4bc9d5aa
florentinobenedictus <florentinobenedictus@gmail.com>
2022-10-29 11:02:54 +0700

    add multiple objects & sphere

--- js/script.js
+++ js/script.js
@@ -12,21 +12,33 @@ const cube = new THREE.Mesh( geometry, material );
 cube.position.set(1, 1, 1);
 scene.add( cube );
 
-// // Ambient Light
-const light = new THREE.AmbientLight(0xFFFFFF, 1);
-scene.add( light );
+const cube2 = new THREE.Mesh( geometry, material );
+cube2.position.set(0, 3, 1);
+scene.add(cube2);
 
-// // HemisphereLight
-// const skyColor = 0x0000FF;  // blue
-// const groundColor = 0xFF0000;  // red
-// const light = new THREE.HemisphereLight(skyColor, groundColor, 1);
+var mysphere = new THREE.SphereGeometry(1, 32, 32);
+var color = new THREE.Color('#ee7800');
+var hex = color.getHex;
+var sphereMaterial = new THREE.MeshLambertMaterial( { color: hex } );
+var sphere = new THREE.Mesh(mysphere, sphereMaterial);
+sphere.position.set(5, 1, 1);
+scene.add( sphere );
+
+// Ambient Light
+// const light = new THREE.AmbientLight(0xFFFFFF, 1);
 // scene.add( light );
 
+// // HemisphereLight
+const skyColor = 0x0000FF;  // blue
+const groundColor = 0xFF0000;  // red
+const light = new THREE.HemisphereLight(skyColor, groundColor, 1);
+scene.add( light );
+
 // DirectionalLight
-// const color = 0xFFFFFF;
+// const color = 0xFF0080;
 // const intensity = 1;
 // const light = new THREE.DirectionalLight(color, intensity);
-// light.position.set(0, -5, 0);
+// light.position.set(0, 5, 0);
 // light.target.position.set(-5, 0, 0);
 // scene.add( light );
 // scene.add(light.target);
@@ -35,14 +47,15 @@ scene.add( light );
 // const color = 0xFFFFFF;
 // const intensity = 1;
 // const light = new THREE.PointLight(color, intensity);
-// light.position.set(0, 5, 0);
+// light.position.set(-5, 0, 0);
 // scene.add( light );
 
-// SpotLight
+// // SpotLight
 // const color = 0x000000;
 // const intensity = 1;
-// const light = new THREE.SpotLight(color, intensity);
-// light.position.set(0, 10, 0);
+// const light = new THREE.SpotLight(color, intensity,0 , 1);
+// light.position.set(0, 5, 0);
+// //light.position.target.set(0, -5, 0);
 // scene.add(light);
 // scene.add(light.target);
 
@@ -56,23 +69,14 @@ renderer.render( scene, camera );
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-// function animate() {
-//     requestAnimationFrame( animate );
-//     cube.rotation.x += 0.01;
-//     cube.rotation.y += 0.01;
-//     cube.rotation.z += 0.01;
-//     renderer.render( scene, camera );
-// }
-// animate();
+function animate() {
+    requestAnimationFrame( animate );
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    cube.rotation.z += 0.01;
+    cube2.rotation.x += 0.01;
+    sphere.rotation.x += 0.01;
+    sphere.rotation.y += 0.01;
+    renderer.render( scene, camera );
+}
+animate();
