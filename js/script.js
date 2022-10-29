0329362dcfe60b4fdc278d114570dadb09860601
florentinobenedictus <florentinobenedictus@gmail.com>
2022-10-29 11:06:16 +0700

    add comments

--- js/script.js
+++ js/script.js
@@ -1,21 +1,26 @@
+// Init scene & camera
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 camera.position.set(2, 1, 5);
 
+// Init renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+// Cube Object
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshPhongMaterial( { color: 0xFFFFFF } );
 const cube = new THREE.Mesh( geometry, material );
 cube.position.set(1, 1, 1);
 scene.add( cube );
 
+// Cube 2 Object
 const cube2 = new THREE.Mesh( geometry, material );
 cube2.position.set(0, 3, 1);
 scene.add(cube2);
 
+// Sphere Object
 var mysphere = new THREE.SphereGeometry(1, 32, 32);
 var color = new THREE.Color('#ee7800');
 var hex = color.getHex;
@@ -28,7 +33,7 @@ scene.add( sphere );
 // const light = new THREE.AmbientLight(0xFFFFFF, 1);
 // scene.add( light );
 
-// // HemisphereLight
+// HemisphereLight
 const skyColor = 0x0000FF;  // blue
 const groundColor = 0xFF0000;  // red
 const light = new THREE.HemisphereLight(skyColor, groundColor, 1);
@@ -50,7 +55,7 @@ scene.add( light );
 // light.position.set(-5, 0, 0);
 // scene.add( light );
 
-// // SpotLight
+// SpotLight
 // const color = 0x000000;
 // const intensity = 1;
 // const light = new THREE.SpotLight(color, intensity,0 , 1);
@@ -59,15 +64,11 @@ scene.add( light );
 // scene.add(light);
 // scene.add(light.target);
 
-
+// Background Color
 // renderer.setClearColor ( 0xffffff )
 
+// Render
 renderer.render( scene, camera );
-
-
-
-
-
 
 function animate() {
     requestAnimationFrame( animate );
